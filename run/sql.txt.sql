11a753cccc2b64777f232812f57d5e2c27fff485
alinegonz <alinegonzz@hotmail.com>
2015-10-12 16:04:29 -0300

    Update sql.txt.sql

--- run/sql.txt.sql
+++ run/sql.txt.sql
@@ -503,5 +503,4 @@ where upper(descricao) = 'ABRIL') as tb_abril,
 
 (select count(*) as qtd_campus from tb_livro l
 join tb_editora e on e.codigo = l.codigo_editora
-where upper(descricao) = 'CAMPUS') as tb_campus;//teste de alteracao
-
+where upper(descricao) = 'CAMPUS') as tb_campus;
